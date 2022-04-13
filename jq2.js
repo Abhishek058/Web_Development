52a8fd9b8d93f7ee036a952d6d92bd858651b9f6
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-13 11:17:49 +0530

    updated

--- jq2.js
+++ jq2.js
@@ -1,4 +1,9 @@
 // $('#learning').hide(3000);
+
 $('#learning').hide(3000, function(){
     console.log("content hidden completed...");
+});
+
+$('#learning').show(4000, function(){
+    console.log("content showen completed...");
 });
