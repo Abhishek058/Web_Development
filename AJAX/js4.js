ba18e731b248aea597d9d2df426f4cb0032d46c7
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-19 10:59:14 +0530

    function bug fixed

---
+++ AJAX/js4.js
@@ -0,0 +1,10 @@
+let getBtn1 = document.querySelector('#btn-primary');
+let getBtn2 = document.querySelector('#btn-success');
+
+getBtn1.addEventListener('click', () => {
+    console.log("You clicked primary putton");
+
+    // const xhr = new XMLHttpRequest();
+
+    // xhr.open('GET', "ajaxServer.txt", true);
+})
