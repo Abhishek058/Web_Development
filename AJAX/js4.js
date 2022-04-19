ca36467048b7203bdf9c1fa05040a0c86ba00801
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-19 11:02:47 +0530

    server created

--- AJAX/js4.js
+++ AJAX/js4.js
@@ -4,7 +4,7 @@ let getBtn2 = document.querySelector('#btn-success');
 getBtn1.addEventListener('click', () => {
     console.log("You clicked primary putton");
 
-    // const xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
 
-    // xhr.open('GET', "ajaxServer.txt", true);
+    xhr.open('GET', "ajaxServer.txt", true);
 })
