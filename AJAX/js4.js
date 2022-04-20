de7a802280787f5c9f9da8c66c25fdbb676312fb
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-20 11:35:24 +0530

    new file created

--- AJAX/js4.js
+++ AJAX/js4.js
@@ -1,34 +1,34 @@
-
 // const http = require('http')
 //Error details
 // console.log(http);
 
-let getBtn1 = document.querySelector('#btn-primary');
+let getBtn1 = document.querySelector("#btn-primary");
 // let getBtn2 = document.querySelector('#btn-success');
 
-getBtn1.addEventListener('click', () => {
-    console.log("You clicked primary putton");
+getBtn1.addEventListener("click", () => {
+  console.log("You clicked primary putton");
 
-    const xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', "ajaxServer.txt", true);
+  xhr.open("GET", "ajaxServer.txt", true);
 
-    xhr.onprogress = function() {
-        console.log("In progress.....");
+  xhr.onprogress = function () {
+    console.log("In progress.....");
+  };
+  xhr.onload = function () {
+    if (this.status == 200) {
+      console.log(this.responseText);
+    } else {
+      console.error("Error occured");
     }
-    xhr.onload = function() {
-        if(this.status == 200){
-            console.log(this.responseText);
-        }
-        else{
-            console.error("Error occured");
-        }
-    }
+  };
 
-    xhr.send();
-    console.log("Taks done");
+  xhr.send();
+  console.log("Taks done");
 
-    xhr.onreadystatechange = function(){
-        console.log("ready state is ", xhr.readyState);
-    }
+  xhr.onreadystatechange = function () {
+    console.log("ready state is ", xhr.readyState);
+  };
+
+  
 });
