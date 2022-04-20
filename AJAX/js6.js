1b7ff738a7f348c4648971bd2f9ed31434a95ce7
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-20 12:31:33 +0530

    js6 updated with POST code

--- AJAX/js6.js
+++ AJAX/js6.js
@@ -6,23 +6,27 @@ dispBtn.addEventListener('click', () => {
     const xhr = new XMLHttpRequest()
 
     // using a rest api
-    xhr.open('GET', 'http://dummy.restapiexample.com/api/v1/employees', true)
+    xhr.open('POSt', 'http://dummy.restapiexample.com/api/v1/create', true);
+
+    xhr.getResponseHeader('Content-type', 'application/json');
 
     xhr.onload = function(){
         if(this.status === 200){
             // parsing the recieved text
-            let obj = JSON.parse(this.responseText)
-            console.log(obj);
+            // let obj = JSON.parse(this.responseText)
+            // console.log(obj);
 
             // publishing dynamically through JS
-            let str = ''
-            let data = obj.data
-            let lst = document.querySelector("#list")
-            for(let i = 0; i < data.length ; i++){
-                str += `<li>${obj.data[i].student_name} </li>`
-            }
-            // rendering onto the screen
-            lst.innerHTML = str
+            // let str = ''
+            // let data = obj.data
+            // let lst = document.querySelector("#list")
+            // for(let i = 0; i < data.length ; i++){
+            //     str += `<li>${obj.data[i].student_name} </li>`
+            // }
+            // // rendering onto the screen
+            // lst.innerHTML = str
+
+            console.log(this.responseText);
         }else{
             console.log("Error occurred");
         }
