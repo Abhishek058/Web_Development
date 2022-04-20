fc25c2b1471bcf9fee49a11421252db077df2d6f
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-20 11:40:44 +0530

    new js updated

--- AJAX/js5.js
+++ AJAX/js5.js
@@ -1,1 +1,13 @@
-const dispBtn = document
+const dispBtn = document.getElementById('btn-success');
+
+dispBtn1.addEventListener("click", () => {
+    console.log("you clicked");
+    xhr.open("GET", "http://dummy.restapiexample.com/api/v1/employees", true);
+
+    xhr.onload = function() {
+        if(this.status === 200){
+            let obj = JSON.parse(this.responseText);
+            console.log(obj);
+        }
+    }
+});
