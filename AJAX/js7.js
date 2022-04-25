6131ca4dc8b3ad3fcb3bb16571b948d7cddaed59
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-04-25 09:50:53 +0530

    new file app added

--- AJAX/js7.js
+++ AJAX/js7.js
@@ -1,8 +1,32 @@
-const sucBtn = document.querySelector('btn-success');
+const sucbtn = document.querySelector('#btn-success')
 
-sucBtn.addEventListener('click', ()=> {
-    console.log("clicked");
-    const xhr = new XMLHttpRequest();
+// using a third party api
+sucbtn.addEventListener('click', () => {
+    console.log("You clicked the display button");
+    const xhr = new XMLHttpRequest()
 
-    xhr.open('POST', )
+    // using a rest api
+    xhr.open('GET', 'https://gorest.co.in/public/v2/posts', true)
+
+    xhr.onload = function(){
+        if(this.status === 200){
+            // parsing the recieved text
+            let obj = JSON.parse(this.responseText)
+            console.log(obj);
+
+            // publishing dynamically through JS
+            let str = ''
+            let data = obj.data
+            let lst = document.querySelector("#list")
+            for(let i = 0; i < obj.length ; i++){
+                str += `<li>${obj[i].title} </li>`
+            }
+            // rendering onto the screen
+            lst.innerHTML = str
+        }else{
+            console.log("Error occurred");
+        }
+    }
+
+    xhr.send()
 })
