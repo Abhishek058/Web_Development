d466c4b4383d24cc2b5d7592c487cb23430c4324
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-04-25 09:34:52 +0530

    bug fixed

--- AJAX/js7.js
+++ AJAX/js7.js
@@ -1,1 +1,8 @@
 const sucBtn = document.querySelector('btn-success');
+
+sucBtn.addEventListener('click', ()=> {
+    console.log("clicked");
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('POST', )
+})
