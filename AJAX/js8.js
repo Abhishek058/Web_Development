dce6e86c900e192da8d98eb7734386496ff43977
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-04-27 11:38:25 +0530

    Style fully added

--- AJAX/js8.js
+++ AJAX/js8.js
@@ -1,3 +1,21 @@
 const user = document.querySelector('#userip');
-const pass = document.querySelector('#subip');
+const pass = document.querySelector('#passip');
 const subtn = document.querySelector('#subip');
+
+document.addEventListener('click', () => {
+    console.log("Checking info");
+    const xhr = new XMLHttpRequest()
+
+    // using a rest api
+    xhr.open('GET', 'json1.json', true)
+
+    xhr.onload = function(){
+        if(){
+            
+        }else{
+            alert("User not found");
+        }
+    }
+
+    xhr.send()
+})
