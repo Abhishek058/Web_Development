5ad0c460152b9f704b2805fae3bc8c97c1b1e6b1
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-03 10:04:50 +0530

    header styling added

--- react-class/src/components/Header.js
+++ react-class/src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-        <h1>Header</h1>
+    <div className='header'>
+        <h1 className='header-h1'>Hello, I am learning React</h1>
     </div>
   )
 }
