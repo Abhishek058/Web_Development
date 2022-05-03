41465906a22a9ba18635fc7a20d30345ee5bdb27
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-03 10:38:24 +0530

    header of react.com created

--- react-class/src/components/Header.js
+++ react-class/src/components/Header.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
-    <div className='header'>
-        <h1 className='header-h1'>Hello, I am learning React</h1>
+    <div className="header">
+      <p className="header-p">
+        Support Ukraine 🇺🇦 <span id="header-span">Help Provide Humanitarian Aid to Ukraine.</span>
+      </p>
+      <div id="nav-bar">
+          <img src={"logo192.png"} alt=""/>
+          <h2 className="header-h2">React</h2>
+          <div className="nav">
+              <p>Doc</p>
+              <p>Tutorial</p>
+              <p>Blog</p>
+              <p>Community</p>
+          </div>
+      <input type="text" placeholder="Search" id="header-search"/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
