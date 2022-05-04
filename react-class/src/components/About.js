9f38b7a3410e0ddf6b397f4c54abcf91ad6e4737
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-04 11:42:19 +0530

    about basic code and basic css added

---
+++ react-class/src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <h1 id="about-h1">React</h1>
+      <p id="about-p1">A JavaScript library for building user interfaces</p>
+      <div id="about-btn">
+        <button id="getstart-btn">Get Started</button>
+        <p id="about-p2">Take Tutorial &gt; </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
