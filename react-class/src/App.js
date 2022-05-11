227f85a88926ce2ea742f1f5939d862047eaf5b2
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-11 11:22:57 +0530

    new section added

--- react-class/src/App.js
+++ react-class/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import About from "./components/About";
+import BasicDisc from "./components/BasicDisc";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <About />
+      <BasicDisc />
     </div>
   );
 }
