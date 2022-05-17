eddfdf14c05229c59a6be1437c6063725da8f0d4
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-17 10:28:20 +0530

    text capitalization function added

--- reactclass2/src/components/State.js
+++ reactclass2/src/components/State.js
@@ -1,19 +1,29 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 function State() {
-    const [text, settext] = useState("hello");
-    const handleOnClick = () =>{
-        console.log("You clicked the button");
-    }
-    const handleOnChange = (event) =>{
-        settext(event.target.value);
-    }
-    return (
+  const [text, settext] = useState("");
+  const handleOnClick = () => {
+    let newTxt = text.toUpperCase();
+    settext(newTxt);
+    console.log(newTxt);
+  };
+  const handleOnChange = (event) => {
+    settext(event.target.value);
+  };
+  return (
     <div>
-        <textarea className="form-control" id="text-area" rows="10" onChange={handleOnChange}></textarea>
-        <button id = "text_btn">Convert</button>
+      <textarea
+        className="form-control"
+        value = {text}
+        id="text-area"
+        rows="10"
+        onChange={handleOnChange}
+      ></textarea>
+      <button id="text_btn" onClick={handleOnClick}>
+        Convert
+      </button>
     </div>
-  )
+  );
 }
 
-export default State
+export default State;
