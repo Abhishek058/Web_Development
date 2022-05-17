d5f16d87650d6d6bea07fed38c20e71b62eca42f
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-17 10:01:16 +0530

    text area and button in state file

--- reactclass2/src/App.js
+++ reactclass2/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavB title="Chitkara University" abouttxt = "AboutCU"/>
+      <State />
     </div>
   );
 }
--- reactclass2/src/components/State.js
+++ reactclass2/src/components/State.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function state() {
-  return (
+function State() {
+    const [text, settext] = useState("hello");
+    const handleOnClick = () =>{
+        console.log("You clicked the button");
+    }
+    const handleOnChange = (event) =>{
+        settext(event.target.value);
+    }
+    return (
     <div>
-        
+        <textarea className="form-control" id="text-area" rows="10" onChange={handleOnChange}></textarea>
+        <button id = "text_btn">Convert</button>
     </div>
   )
 }
 
-export default state
+export default State
