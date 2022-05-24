e9e8d374981a460833f25919161ee09ae02e30a6
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-24 09:36:10 +0530

    three more buttons added

--- reactclass2/src/components/State.js
+++ reactclass2/src/components/State.js
@@ -14,6 +14,24 @@ function State() {
   const handleOnChange = (event) => {
     settext(event.target.value);
   };
+  const replaceStr = () => {
+    let repStr = text.replace("CU", "TU");
+    settext(repStr);
+  };
+
+  const charatStr = () => {
+    let splStr = text.charAt(5);
+    settext(splStr);
+  };
+  const repeatStr = () => {
+    let repeStr = text.repeat(3);
+    settext(repeStr);
+  };
+
+  const handleOnClear = () => {
+    let newText = "";
+    settext(newText);
+  };
   return (
     <div>
       <textarea
@@ -23,9 +41,27 @@ function State() {
         rows="10"
         onChange={handleOnChange}
       ></textarea>
+      <div>
       <button id="text_btn" onClick={handleOnClick}>
         Convert
       </button>
+      <button className="btn" id="text_btn" onClick={replaceStr}>
+        Replace
+      </button>
+      <button className="btn" id="text_btn" onClick={handleOnClear}>
+        Clear
+      </button>
+      <button className="btn" id="text_btn" onClick={charatStr}>
+        Char At
+      </button>
+      <button className="btn" id="text_btn" onClick={repeatStr}>
+        Repeat
+      </button>
+      </div>
+      <div className="container my-3">
+        <p>Number of words written : {text.split("").length}</p>
+        <p>No. of letters in sentence written : {text.split("").length}</p>
+      </div>
     </div>
   );
 }
