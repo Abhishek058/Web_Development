bf9a013643234ead066d1ecb028477c7c7b86dd3
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-06-01 11:35:44 +0530

    object created

--- reactclass3/src/App.js
+++ reactclass3/src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="bg-blue-400">
-      <h1>HELLO</h1>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      students: [
+        {
+          Rollno: 58,
+          name: "Abhishek",
+        },
+        {
+          Rollno: 34,
+          name: "Abhay",
+        },
+        {
+          Rollno: 57,
+          name: "Abhiraj",
+        },
+        {
+          Rollno: 70,
+          name: "Abhishek",
+        },
+        
+        {
+          Rollno: 2,
+          name: "Aaditya",
+        },
+      ],
+    };
+  }
+  render() {
+    return <div className="bg-green-500">
+      <div class></div>
+    </div>;
+  }
 }
-
-export default App;
